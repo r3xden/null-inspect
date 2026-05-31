4a7ab15f81b79cbf94b0a0de095ed70f7e588f70
Rexden <156419201+r3xden@users.noreply.github.com>
2026-05-31 19:49:32 +0530

    Update inspectError.js

--- src/inspectors/inspectError.js
+++ src/inspectors/inspectError.js
@@ -1,1 +1,16 @@
+/**
+ * null-inspect
+ * Error Inspector
+ */
 
+import ErrorAnalyzer from "../analyzers/ErrorAnalyzer.js";
+
+export default function inspectError(
+    error,
+) {
+
+    return ErrorAnalyzer.analyze(
+        error,
+    );
+
+}
