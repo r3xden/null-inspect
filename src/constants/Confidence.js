41d75183a80e4941677be1f09b2d3f15d92a738e
Rexden <156419201+r3xden@users.noreply.github.com>
2026-05-31 19:42:48 +0530

    Update Confidence.js

--- src/constants/Confidence.js
+++ src/constants/Confidence.js
@@ -1,1 +1,19 @@
+/**
+ * null-inspect
+ * Confidence Levels
+ */
 
+const Confidence = {
+
+    LOW:
+        "Low",
+
+    MEDIUM:
+        "Medium",
+
+    HIGH:
+        "High",
+
+};
+
+export default Confidence;
