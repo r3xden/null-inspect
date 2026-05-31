fa6dbe538e8dc62d4dcec06436ef021b21aa293f
Rexden <156419201+r3xden@users.noreply.github.com>
2026-05-31 19:43:28 +0530

    Update InspectionReport.js

--- src/models/InspectionReport.js
+++ src/models/InspectionReport.js
@@ -1,1 +1,67 @@
+/**
+ * null-inspect
+ * Inspection Report Model
+ */
 
+export default class InspectionReport {
+
+    constructor({
+
+        code,
+
+        category,
+
+        message,
+
+        issue,
+
+        cause,
+
+        fix,
+
+        location = {
+
+            file: null,
+
+            line: null,
+
+            column: null,
+
+        },
+
+        severity,
+
+        confidence,
+
+    }) {
+
+        this.code =
+            code;
+
+        this.category =
+            category;
+
+        this.message =
+            message;
+
+        this.issue =
+            issue;
+
+        this.cause =
+            cause;
+
+        this.fix =
+            fix;
+
+        this.location =
+            location;
+
+        this.severity =
+            severity;
+
+        this.confidence =
+            confidence;
+
+    }
+
+}
