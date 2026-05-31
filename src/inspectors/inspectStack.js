48b716431f57cd2e9a34397ed809857fe7b41edc
Rexden <156419201+r3xden@users.noreply.github.com>
2026-05-31 19:50:11 +0530

    Update inspectStack.js

--- src/inspectors/inspectStack.js
+++ src/inspectors/inspectStack.js
@@ -1,1 +1,16 @@
+/**
+ * null-inspect
+ * Stack Inspector
+ */
 
+import StackAnalyzer from "../analyzers/StackAnalyzer.js";
+
+export default function inspectStack(
+    stack,
+) {
+
+    return StackAnalyzer.analyze(
+        stack,
+    );
+
+}
