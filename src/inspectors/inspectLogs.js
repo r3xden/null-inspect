91863cb773c3741bf3f4c915ab66aae388dfebd3
Rexden <156419201+r3xden@users.noreply.github.com>
2026-05-31 19:50:47 +0530

    Update inspectLogs.js

--- src/inspectors/inspectLogs.js
+++ src/inspectors/inspectLogs.js
@@ -1,1 +1,16 @@
+/**
+ * null-inspect
+ * Log Inspector
+ */
 
+import LogAnalyzer from "../analyzers/LogAnalyzer.js";
+
+export default function inspectLogs(
+    logs,
+) {
+
+    return LogAnalyzer.analyze(
+        logs,
+    );
+
+}
