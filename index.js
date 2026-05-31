96fb06233ae956d910f7f5ba868120bf35ff6f9d
Rexden <156419201+r3xden@users.noreply.github.com>
2026-05-31 19:51:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+/**
+ * null-inspect
+ * Package Exports
+ */
 
+export {
+    default as inspectError,
+} from "./inspectors/inspectError.js";
+
+export {
+    default as inspectStack,
+} from "./inspectors/inspectStack.js";
+
+export {
+    default as inspectLogs,
+} from "./inspectors/inspectLogs.js";
